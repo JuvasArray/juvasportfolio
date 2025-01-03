cd3593910f2f23e60f7de04f65edba5eca197a42
juvas <achouraithamiche40@gmail.com>
2025-01-03 13:35:41 -0500

    Improved the application

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
         PUBLISHED = 'published'
 
     title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique_for_date='publish')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -28,7 +28,7 @@ class Post(models.Model):
     published = PublishedManager()
 
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[str(self.id)])
+        return reverse('blog:post_detail', args=[str(self.slug)])
     
     class Meta:
         ordering = ['-publish']
@@ -37,3 +37,20 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
+    
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
-
-from blog.models import Post
+from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from blog.forms import EmailPostForm
+from blog.models import Post
+from django.views.decorators.http import require_POST
+from blog.forms import CommentForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     template_name = 'blog/post/list.html'
     paginate_by = 3
-
+    
     
 class PostDetailView(DetailView):
     model = Post
@@ -18,10 +24,43 @@ class PostDetailView(DetailView):
     template_name = 'blog/post/detail.html'
     
     def get_object(self, queryset=None):
-        id = self.kwargs.get('id')
-        return get_object_or_404(Post, id=id)
+        post = self.kwargs.get('post')
+        return get_object_or_404(Post, slug=post)
     
     def get_context_data(self, **kwargs):
         context = super(PostDetailView, self).get_context_data(**kwargs)
         context['post'] = self.object
+        context['comments'] = self.object.comments.filter(active=True)
+        context['comment_form'] = CommentForm()
         return context
+    
+
+def post_share(request, post_id, *args, **kwargs):
+    post = get_object_or_404(Post, id=kwargs.get('post_id'), status=Post.Status.PUBLISHED)
+    sent = False
+    
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, form_email=None, recipient_list=[cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+    
+    
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return HttpResponseRedirect(reverse('blog:post_detail', args=[post.slug]))
